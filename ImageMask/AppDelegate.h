35398c26b9fcd76da04f9a60fac48186694fb5bc
Xmanzn <898287572@qq.com>
2016-03-13 15:26:09 +0800

    Initial Commit

---
+++ ImageMask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageMask
+//
+//  Created by Macx on 16/3/13.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageMask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageMask
+//
+//  Created by Macx on 16/3/13.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
